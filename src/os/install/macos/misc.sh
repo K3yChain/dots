b3337eeb49b6dce9c668e84ccedcccfa2380171b
Nick <nick@logitek.me>
2016-08-20 07:58:31 -0700

    Update misc.sh

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -15,18 +15,18 @@ install_apps() {
     brew_install "Jaksta" "jaksta" "caskroom/cask" "cask"
     brew_install "VMWare Fusion" "vmware-fusion" "caskroom/cask" "cask"
     brew_install "Remote Desktop Manager" "remote-desktop-manager" "caskroom/cask" "cask"
-    brew_install "AirMail - Beta" "airmail-beta" "caskroom/cask" "cask"
+    brew_install "AirMail (Beta)" "airmail-beta" "caskroom/cask" "cask"
     brew_install "BitTorrent Sync" "bittorrent-sync" "caskroom/cask" "cask"
     brew_install "Cyberduck" "cyberduck" "caskroom/cask" "cask"
     brew_install "AppCleaner" "appcleaner" "caskroom/cask" "cask"
     brew_install "iTerm2" "iterm2" "caskroom/cask" "cask"
     brew_install "TextWrangler" "textwrangler" "caskroom/cask" "cask"
-    brew_install "SLACK" "slack" "caskroom/cask" "cask"
+    brew_install "#Slack" "slack" "caskroom/cask" "cask"
     brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
     brew_install "Firefox" "firefox" "caskroom/cask" "cask"
     brew_install "Flash" "flash" "caskroom/cask" "cask"
     brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
-    brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
+    brew_install "The Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
