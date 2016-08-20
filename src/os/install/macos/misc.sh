3031bc91f488e9f1ee50ee131a136d9c31ae576b
Nick <nick@logitek.me>
2016-08-20 06:31:22 -0700

    Update misc.sh

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -7,13 +7,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 install_apps() {
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     brew_install "htop" "htop"
-    
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "CrashPlan" "crashplan" "caskroom/cask" "cask"
     brew_install "DropBox" "dropbox" "caskroom/cask" "cask"
     brew_install "Jaksta" "jaksta" "caskroom/cask" "cask"
@@ -26,70 +23,18 @@ install_apps() {
     brew_install "Growl Notify" "growlnotify" "caskroom/cask" "cask"
     brew_install "iTerm2" "iterm2" "caskroom/cask" "cask"
     brew_install "TextWrangler" "textwrangler" "caskroom/cask" "cask"
-    brew_install "Wondershare Video Converter Ultimate" "wondershare-video-converter-ultimate" "caskroom/cask" "cask"
     brew_install "SLACK" "slack" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+    brew_install "Little Snitch" "little-snitch" "caskroom/cask" "cask"
     brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "FFmpeg" "ffmpeg"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Firefox" "firefox" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Flash" "flash" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Git" "git"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # `Safari Technology Preview` requires macOS 10.11.4 or la
-    # https://github.com/alrra/dotfiles/issues
-
-    brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "ShellCheck" "shellcheck"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "tmux" "tmux"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Vim" "vim --override-system-vi"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install "VirtualBox" "virtualbox" "caskroom/cask" "cask"   
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     brew_install "Web Font Tools: TTF/OTF → WOFF (Zopfli)" "sfnt2woff-zopfli" "bramstein/webfonttools"
     brew_install "Web Font Tools: TTF/OTF → WOFF" "sfnt2woff" "bramstein/webfonttools"
     brew_install "Web Font Tools: WOFF2" "woff2" "bramstein/webfonttools"
@@ -98,16 +43,25 @@ install_apps() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # brew_install "WebKit" "webkit-nightly" "caskroom/versions" "cask"
+# Disabled Apps
 
+    # `Safari Technology Preview` requires macOS 10.11.4 or la
+    # brew_install "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
+    
+    # brew_install "LibreOffice" "libreoffice" "caskroom/cask" "cask" 
+    # brew_install "VirtualBox" "virtualbox" "caskroom/cask" "cask"   
+    # brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
+    # brew_install "Wondershare Video Converter Ultimate" "wondershare-video-converter-ultimate" "caskroom/cask" "cask"
+  
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
-    print_info " Miscellaneous"
+    print_info " Miscellaneous Apps & Features"
 
     install_apps
 
