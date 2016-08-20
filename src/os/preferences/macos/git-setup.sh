866a57261ee0dfc0bde3aa5cf0fa0e10d15b6dfd
Nick Raccioppi <nick@logitek.me>
2016-08-20 07:53:24 -0700

    fixing git setup script

---
+++ src/os/preferences/macos/git-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "../../install/macos/utils.sh"
+
+main() {
+
+    print_info " Nick's GIT Setup"
+
+}
+
+main
+
+git config --global user.email "nick@logitek.me"
+print_success "Setting GIT Email: nick@logitek.me"
+sleep 1
+git config --global user.name "Nick Raccioppi"
+print_success "Setting GIT Username: Nick Raccioppi"
+sleep 1
