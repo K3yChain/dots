e78c6a6921b760fbeae4d9dbd762168c547ba833
Nick <nick@logitek.me>
2016-08-20 06:33:23 -0700

    Create bash-apps.sh

---
+++ src/os/install/macos/bash-apps.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_apps() {
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    brew_install "Bartender" "bartender" "caskroom/cask" "cask"
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    brew_install "Alfred" "alfred" "caskroom/cask" "cask"
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    brew_install "BetterTouchTool" "bettertouchtool" "caskroom/cask" "cask"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    brew_install "iStats Menus" "istat-menus" "caskroom/cask" "cask"
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    brew_install "Little Snitch" "little-snitch" "caskroom/cask" "cask"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_info " UI Related Apps"
+
+    install_apps
+
+    printf "\n"
+    brew_cleanup
+
+}
+
+main
