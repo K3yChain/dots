efdac6cdc3b7c46aaf72795b690972a19900a880
Nick <nick@logitek.me>
2016-10-28 23:47:57 -0700

    Update bash-apps.sh

--- src/os/install/macos/bash-apps.sh
+++ src/os/install/macos/bash-apps.sh
@@ -11,7 +11,7 @@ install_apps() {
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     brew_install "Growl Notify" "growlnotify" "caskroom/cask" "cask"
-    # brew_install "FFmpeg" "ffmpeg"
+    brew_install "FFmpeg" "ffmpeg"
     brew_install "Git" "git"
     brew_install "ShellCheck" "shellcheck"
     brew_install "tmux" "tmux"
