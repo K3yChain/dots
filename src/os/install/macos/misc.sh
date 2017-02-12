51a74f3a6ec21493bd473b219761d5f43e201de5
Nick <nick@logitek.me>
2017-02-12 01:32:27 -0800

    Update misc.sh

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -13,7 +13,6 @@ install_apps() {
 	brew_install "DropBox" "dropbox" "caskroom/cask" "cask"
     brew_install "Jaksta" "jaksta" "caskroom/cask" "cask"
     brew_install "VMWare Fusion" "vmware-fusion" "caskroom/cask" "cask"
-    brew_install "AirMail (Beta)" "Caskroom/versions/airmail-beta" "caskroom/cask" "cask"
     brew_install "Resilio Sync (Formerly Bittorrent Sync)" "resilio-sync" "caskroom/cask" "cask"
     brew_install "Cyberduck" "cyberduck" "caskroom/cask" "cask"
     brew_install "AppCleaner" "appcleaner" "caskroom/cask" "cask"
@@ -23,11 +22,10 @@ install_apps() {
     brew_install "Chrome" "google-chrome" "caskroom/cask" "cask"
     brew_install "Firefox" "firefox" "caskroom/cask" "cask"
     brew_install "Flash" "flash-player" "caskroom/cask" "cask"
-    brew_install "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
     brew_install "The Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
 	brew_install "VLC" "vlc" "caskroom/cask" "cask"
 	brew_install "Spotify" "spotify" "caskroom/cask" "cask"
-
+	brew_install "CrashPlan" "crashplan" "caskroom/cask" "cask"
 	
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -41,8 +39,6 @@ install_apps() {
     # brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
 	# brew_install "Remote Desktop Manager" "remote-desktop-manager" "caskroom/cask" "cask"
 	# brew_install "Wondershare Video Converter Ultimate" "wondershare-video-converter-ultimate" "caskroom/cask" "cask"
-	# brew_install "CrashPlan" "crashplan" "caskroom/cask" "cask"
-
 
 
     
